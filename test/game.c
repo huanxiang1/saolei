ee34788092db14c0901d3232757d554f182d3b1c
huanxiang1 <gaohang100@163.com>
2024-06-20 13:51:53 +0800

    扫雷

---
+++ test/game.c
@@ -0,0 +1,128 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+
+
+
+//初始化雷盘
+void Initboard(char board[ROWS][COLS], int rows, int cols, char set)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < cols; j++)
+		{
+			board[i][j] = set;
+		}
+	}
+}
+
+
+//打印棋盘
+void DisplayBoard(char board[ROWS][COLS], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	//打印列号
+	for (i = 0; i <= row; i++)
+	{
+		if (i == 0)
+			printf("+ ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	for (i = 1; i <= row; i++)
+	{
+		//打印行号
+		printf("%d ", i);
+		for (j = 1; j <=col; j++)
+		{
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+//布置雷
+void SetMine(char board[ROWS][COLS], int row, int col)
+{
+	//雷的个数
+	int count = EASY_COUNT;
+	while (count)
+	{
+		int x = rand() % row + 1;//生成1-row
+		int y = rand() % col + 1;//生成1-row
+		if (board[x][y] == '0')
+		{
+			board[x][y] = '1';
+			count--;
+		}
+	}
+}
+
+//'1'-'0'=1
+//'3'-'0'=3
+
+int get_mine_count(char mine[ROWS][COLS],int x, int y)
+{
+		return mine[x - 1][y] +
+		mine[x - 1][y - 1] +
+		mine[x][y - 1] +
+		mine[x + 1][y - 1] +
+		mine[x + 1][y] +
+		mine[x + 1][y + 1] +
+		mine[x][y + 1] +
+		mine[x - 1][y + 1] - 8 * '0';
+}
+
+//排雷
+void FineMine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	int win = 0;
+	while(win<row*col-EASY_COUNT)
+	{
+		printf("Please enter the coordinates of the demining:>");
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			//坐标合法
+			//1.踩雷了
+			if (mine[x][y] == '1')
+			{
+				printf("boom!!!!\n");
+				DisplayBoard(mine, row, col);
+				break;
+			}
+			//2.不是雷
+			else
+			{
+				//计算x，y坐标周围有几个雷
+				int count = get_mine_count(mine, x, y);
+				if(count == 0)
+				{
+					show[x][y] = ' ';				
+				}
+				else
+				{
+					show[x][y] = count + '0';
+					
+				}
+				DisplayBoard(show, row, col);
+				win++;
+			}
+		}
+		else
+		{
+			printf("the number is illeagle,please input again!\n");
+			break;
+		}
+	}
+	if (win == row * col - EASY_COUNT)
+	{
+		printf("you are the winner!\n");
+		DisplayBoard(mine, row, col);
+	}
+}
---
+++ test/test.c
@@ -0,0 +1,68 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+
+
+
+void menu()
+{
+	printf("*****************************\n");
+	printf("***        1.play         ***\n");
+	printf("***        0.exit         ***\n");
+	printf("*****************************\n");
+}
+
+
+
+void game()
+{
+	//雷的信息存储
+	//1.布置好的雷的信息
+	char mine[ROWS][COLS] = { 0 };//11*11
+	//2.排查出雷的信息
+	char show[ROWS][COLS] = { 0 };
+	//初始化
+	Initboard(mine, ROWS, COLS, '0');
+	Initboard(show, ROWS, COLS, '*');
+
+	//打印棋盘
+	//DisplayBoard(mine, ROW, COL);
+	DisplayBoard(show, ROW, COL);
+
+	//布置雷
+	SetMine(mine, ROW, COL);
+	//DisplayBoard(mine, ROW, COL);
+
+	//扫雷
+	FineMine(mine,show,ROW,COL);
+}
+
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do 
+	{
+		menu();
+		printf("Please choose:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("See you next time!\n");
+			break;
+		default:
+			printf("Wrong choose,please choose again!\n");
+			break;
+		}
+	} while (input);
+}
+
+
+int main()
+{
+	test();
+	return 0;
+}
